40b84ca2cf2fc9a769e18be40410e498bc1f6ee3
novilen.gov@gmail.com <novilen.gov@gmail.com>
2015-06-05 03:00:02 +0900

    First commit

---
+++ Keipass-a/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.AxJVLink1 = New AxJVDTLabLib.AxJVLink()
+        Me.mainMenu = New System.Windows.Forms.MenuStrip()
+        Me.mnuConfig = New System.Windows.Forms.ToolStripMenuItem()
+        Me.mnuConfJV = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ProgressBar = New System.Windows.Forms.ToolStripProgressBar()
+        Me.BottomLabel = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.btnGetJVData = New System.Windows.Forms.Button()
+        Me.rtbData = New System.Windows.Forms.RichTextBox()
+        Me.btnGetJVOdds = New System.Windows.Forms.Button()
+        Me.ListRace = New System.Windows.Forms.ListBox()
+        Me.btnShowRace = New System.Windows.Forms.Button()
+        CType(Me.AxJVLink1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.mainMenu.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'AxJVLink1
+        '
+        Me.AxJVLink1.Enabled = True
+        Me.AxJVLink1.Location = New System.Drawing.Point(435, 29)
+        Me.AxJVLink1.Name = "AxJVLink1"
+        Me.AxJVLink1.OcxState = CType(resources.GetObject("AxJVLink1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxJVLink1.Size = New System.Drawing.Size(36, 32)
+        Me.AxJVLink1.TabIndex = 0
+        '
+        'mainMenu
+        '
+        Me.mainMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuConfig})
+        Me.mainMenu.Location = New System.Drawing.Point(0, 0)
+        Me.mainMenu.Name = "mainMenu"
+        Me.mainMenu.Size = New System.Drawing.Size(495, 26)
+        Me.mainMenu.TabIndex = 1
+        Me.mainMenu.Text = "MenuStrip1"
+        '
+        'mnuConfig
+        '
+        Me.mnuConfig.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuConfJV})
+        Me.mnuConfig.Name = "mnuConfig"
+        Me.mnuConfig.Size = New System.Drawing.Size(62, 22)
+        Me.mnuConfig.Text = "設定(&C)"
+        '
+        'mnuConfJV
+        '
+        Me.mnuConfJV.Name = "mnuConfJV"
+        Me.mnuConfJV.Size = New System.Drawing.Size(181, 22)
+        Me.mnuConfJV.Text = "JV-Linkの設定(&J)..."
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProgressBar, Me.BottomLabel})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 587)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(495, 23)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(100, 17)
+        Me.ProgressBar.Step = 1
+        '
+        'BottomLabel
+        '
+        Me.BottomLabel.Name = "BottomLabel"
+        Me.BottomLabel.Size = New System.Drawing.Size(136, 18)
+        Me.BottomLabel.Text = "ToolStripStatusLabel1"
+        '
+        'btnGetJVData
+        '
+        Me.btnGetJVData.BackColor = System.Drawing.Color.SteelBlue
+        Me.btnGetJVData.ForeColor = System.Drawing.Color.White
+        Me.btnGetJVData.Location = New System.Drawing.Point(8, 29)
+        Me.btnGetJVData.Name = "btnGetJVData"
+        Me.btnGetJVData.Size = New System.Drawing.Size(98, 32)
+        Me.btnGetJVData.TabIndex = 3
+        Me.btnGetJVData.Text = "データ取得"
+        Me.btnGetJVData.UseVisualStyleBackColor = False
+        '
+        'rtbData
+        '
+        Me.rtbData.Location = New System.Drawing.Point(8, 185)
+        Me.rtbData.Name = "rtbData"
+        Me.rtbData.Size = New System.Drawing.Size(475, 400)
+        Me.rtbData.TabIndex = 4
+        Me.rtbData.Text = ""
+        Me.rtbData.WordWrap = False
+        '
+        'btnGetJVOdds
+        '
+        Me.btnGetJVOdds.BackColor = System.Drawing.Color.SteelBlue
+        Me.btnGetJVOdds.ForeColor = System.Drawing.Color.White
+        Me.btnGetJVOdds.Location = New System.Drawing.Point(112, 29)
+        Me.btnGetJVOdds.Name = "btnGetJVOdds"
+        Me.btnGetJVOdds.Size = New System.Drawing.Size(98, 32)
+        Me.btnGetJVOdds.TabIndex = 5
+        Me.btnGetJVOdds.Text = "オッズ取得"
+        Me.btnGetJVOdds.UseVisualStyleBackColor = False
+        '
+        'ListRace
+        '
+        Me.ListRace.FormattingEnabled = True
+        Me.ListRace.ItemHeight = 12
+        Me.ListRace.Location = New System.Drawing.Point(8, 67)
+        Me.ListRace.Name = "ListRace"
+        Me.ListRace.Size = New System.Drawing.Size(198, 112)
+        Me.ListRace.TabIndex = 6
+        '
+        'btnShowRace
+        '
+        Me.btnShowRace.BackColor = System.Drawing.Color.SteelBlue
+        Me.btnShowRace.ForeColor = System.Drawing.Color.White
+        Me.btnShowRace.Location = New System.Drawing.Point(216, 29)
+        Me.btnShowRace.Name = "btnShowRace"
+        Me.btnShowRace.Size = New System.Drawing.Size(98, 32)
+        Me.btnShowRace.TabIndex = 7
+        Me.btnShowRace.Text = "レース一覧"
+        Me.btnShowRace.UseVisualStyleBackColor = False
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(495, 610)
+        Me.Controls.Add(Me.btnShowRace)
+        Me.Controls.Add(Me.ListRace)
+        Me.Controls.Add(Me.btnGetJVOdds)
+        Me.Controls.Add(Me.rtbData)
+        Me.Controls.Add(Me.btnGetJVData)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.AxJVLink1)
+        Me.Controls.Add(Me.mainMenu)
+        Me.MainMenuStrip = Me.mainMenu
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        CType(Me.AxJVLink1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.mainMenu.ResumeLayout(False)
+        Me.mainMenu.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents AxJVLink1 As AxJVDTLabLib.AxJVLink
+    Friend WithEvents mainMenu As System.Windows.Forms.MenuStrip
+    Friend WithEvents mnuConfig As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuConfJV As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents btnGetJVData As System.Windows.Forms.Button
+    Friend WithEvents rtbData As System.Windows.Forms.RichTextBox
+    Friend WithEvents btnGetJVOdds As System.Windows.Forms.Button
+    Friend WithEvents ListRace As System.Windows.Forms.ListBox
+    Friend WithEvents btnShowRace As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar As System.Windows.Forms.ToolStripProgressBar
+    Friend WithEvents BottomLabel As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ Keipass-a/Form1.vb
@@ -0,0 +1,204 @@
+﻿Public Class frmMain
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Try
+            Dim sid As String
+            Dim IReturnCode As Long
+
+            '引数設定
+            sid = "Keipass"
+
+            'JV Link初期化
+            IReturnCode = Me.AxJVLink1.JVInit(sid)
+
+            If IReturnCode <> 0 Then
+                MsgBox("JVinitエラー コード:" & IReturnCode & "：", MessageBoxIcon.Error)
+                Me.Cursor = System.Windows.Forms.Cursor.Current
+
+            End If
+
+            'コマンドライン引数取得
+            If Environment.GetCommandLineArgs.GetValue(1).ToString = "A" Then
+                Dim Races As New ArrayList
+                Races = GetJVRace()
+            Else
+
+            End If
+        Catch ex As Exception
+
+
+        End Try
+    End Sub
+    Private Sub mnuConfJV_Click(sender As Object, e As EventArgs) Handles mnuConfJV.Click
+        Try
+            'リターンコード
+            Dim IReturnCode As Long
+
+            '設定画面表示
+            IReturnCode = AxJVLink1.JVSetUIProperties
+
+            If IReturnCode <> 0 Then
+                MsgBox("JVSetUIPropertiesエラー()コード:" & IReturnCode & "：", MessageBoxIcon.Error)
+            End If
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub btnGetJVData_Click(sender As Object, e As EventArgs) Handles btnGetJVData.Click
+
+        Dim Races As New ArrayList
+        Dim RaceInfo As JV_RA_RACE
+        Dim strRaceID As String
+
+        Races = GetJVRace()
+
+        ProgressBar.Maximum = Races.Count
+        ProgressBar.Minimum = 0
+        ProgressBar.Step = 1
+        For Each RaceInfo In Races
+            'データ表示
+            strRaceID = RaceInfo.id.Year & RaceInfo.id.MonthDay & RaceInfo.id.JyoCD & RaceInfo.id.Kaiji & RaceInfo.id.Nichiji & RaceInfo.id.RaceNum
+            ListRace.Items.Add(strRaceID)
+            rtbData.AppendText("年:" & RaceInfo.id.Year & "月日:" & RaceInfo.id.MonthDay & "場:" & RaceInfo.id.JyoCD & _
+                               "回次:" & RaceInfo.id.Kaiji & "日次:" & RaceInfo.id.Nichiji & "R:" & RaceInfo.id.RaceNum & _
+                               "レース名:" & RaceInfo.RaceInfo.Ryakusyo10 & vbCrLf)
+            ProgressBar.PerformStep()
+        Next
+
+    End Sub
+    Private Function GetJVRace()
+        Try
+            Dim Races As New ArrayList
+            Dim strBuff = New String(vbNullChar, 110000)
+            Races = JVGet_common("stored", "RACE", "20150510000000", "1")
+
+            Return Races
+        Catch ex As Exception
+            Return "Error"
+        End Try
+    End Function
+    Private Function GetJVOdds()
+        Try
+            Dim strBuff = New String(vbNullChar, 110000)
+            Dim arOdds As New ArrayList
+
+            arOdds = JVGet_common("RT", "0B30", ListRace.SelectedItems.Item(0))
+            Return arOdds
+
+        Catch ex As Exception
+            Return "Error"
+        End Try
+    End Function
+    Private Sub btnGetJVOdds_Click(sender As Object, e As EventArgs) Handles btnGetJVOdds.Click
+        Dim OddsInfo As JV_O1_ODDS_TANFUKUWAKU  'オッズ詳細情報構造体
+        Dim arOdds As New ArrayList
+
+        arOdds = GetJVOdds()
+        OddsInfo = arOdds(0)
+
+        For Each eachOdds In OddsInfo.OddsFukusyoInfo
+            'データ表示
+            rtbData.AppendText("馬番:" & eachOdds.Umaban & "/最高:" & eachOdds.OddsHigh & vbCrLf)
+        Next
+    End Sub
+    Private Function JVGet_common(strDataType As String, strDataSpec As String, strFromTime As String, Optional lOption As Long = 0) As Object
+        'strDataType: JVデータタイプ
+        'strDataSpec: JVOpenファイル識別子
+        'strFromTime: JVOpen データ提供日付
+        'lOption: JVOpen オプション
+
+        Dim objReturn As New ArrayList
+        Dim IReturnCode As Long
+        Try
+            Dim lReadCount As Long      'JV Link戻り値
+            Dim lDownloadCount As Long  'JV Open:総ダウンロードファイル数
+            Dim strLastFileTimestamp As String 'JV Open:最新ファイルのタイムスタンプ
+            Dim strErrMsg As String
+
+            Const lBuffSize As Long = 110000    'JVRead:データ格納バッファサイズ
+            Const lNameSize As Integer = 256    'JVRead:ファイル名サイズ
+            Dim strBuff As String               'JVRead:データ格納バッファ
+            Dim strFileName As String           'JVRead:ダウンロードファイル名
+
+            Dim RaceInfo As JV_RA_RACE          'レース詳細情報構造体
+            Dim OddsInfo As JV_O1_ODDS_TANFUKUWAKU  'オッズ詳細情報構造体
+
+            If strDataType = "stored" Then
+                'JV Link 蓄積型データダウンロード処理
+                IReturnCode = Me.AxJVLink1.JVOpen(strDataSpec, strFromTime, lOption, lReadCount, lDownloadCount, strLastFileTimestamp)
+                strErrMsg = "戻り値:" & IReturnCode & vbCrLf & "読み込みファイル数：" & lReadCount & vbCrLf & _
+                        "ダウンロードファイル数：" & lDownloadCount & vbCrLf & "タイムスタンプ：" & strLastFileTimestamp
+                ProgressBar.Value = Me.AxJVLink1.JVStatus()
+                ProgressBar.Maximum = lDownloadCount
+                ProgressBar.Minimum = 0
+                ProgressBar.Step = 1
+            Else
+                'JV Link リアルタイムデータダウンロード処理
+                IReturnCode = Me.AxJVLink1.JVRTOpen(strDataSpec, strFromTime)
+                strErrMsg = "戻り値:" & IReturnCode
+                lReadCount = 1
+            End If
+
+            If IReturnCode = 0 Then
+                MsgBox(strErrMsg)
+
+                If lReadCount > 0 Then
+                    Do
+                        'バッファ作成
+                        strBuff = New String(vbNullChar, lBuffSize)
+                        strFileName = New String(vbNullChar, lNameSize)
+                        'JVReadで1行読み込み
+                        IReturnCode = Me.AxJVLink1.JVRead(strBuff, lBuffSize, strFileName)
+
+
+                        'リターンコード分岐
+                        Select Case IReturnCode
+                            Case 0  '全ファイル読み込み終了
+                                Exit Do
+                            Case -1 'ファイル切り替わり
+                            Case -3 'ダウンロード中
+                            Case -201 'JV Init未了
+                                MsgBox("JV Initが行われていません。")
+                                Exit Do
+                            Case -203 'Openされていない
+                                MsgBox("JVOpenが行われていません。")
+                                Exit Do
+                            Case -503  'ファイルが存在しない
+                                MsgBox(strFileName & "が存在しません。")
+                                Exit Do
+                            Case Is > 0 '正常読み込み
+                                'レコード種別IDの識別
+                                Select Case Mid(strBuff, 1, 2)
+                                    Case "RA"
+                                        If strDataSpec = "RACE" Then
+                                            RaceInfo.SetData(strBuff)
+                                            objReturn.Add(RaceInfo)
+                                        End If
+
+                                    Case "O1"
+                                        If strDataSpec = "0B30" Then
+                                            OddsInfo.SetData(strBuff)
+                                            objReturn.Add(OddsInfo)
+                                        End If
+                                End Select
+                        End Select
+                    Loop While (1)
+                End If
+            End If
+
+        Catch ex As Exception
+            Debug.WriteLine(Err.Description)
+            Exit Function
+        End Try
+
+        'JV Link処理終了
+        IReturnCode = Me.AxJVLink1.JVClose
+        If IReturnCode <> 0 Then
+            MsgBox("JVCloseエラー：" & IReturnCode)
+        End If
+        MsgBox("読み込み処理終了")
+        Return objReturn
+    End Function
+
+End Class
